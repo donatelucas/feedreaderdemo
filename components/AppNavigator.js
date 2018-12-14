1e679b477de667233153cf4bd4de5b35b6fd204a
Tyler Nguyen <tylernguyen@Tylers-MacBook-Air.local>
2018-12-14 16:27:22 -0600

    build

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -1,19 +1,24 @@
 import { createStackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 //app screens
-import Card from './Card';
+//import Card from './Card';
+import Home from './Home';
 
 //Every time a new screen is added, import it here 
 const AppNavigator = createStackNavigator({
-        Card: { 
-            screen: Card,
+        // Card: { 
+        //     screen: Card,
+        //     navigationOptions: ({ navigation }) => ({ header: null, })
+        // },
+        Home: { 
+            screen: Home,
             navigationOptions: ({ navigation }) => ({ header: null, })
         },
             
     },
 
     {
-        initialRouteName:'Card',
+        initialRouteName:'Home',
     }
 );
 
---
+++ components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+export default class Home extends React.Component {
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    ); 
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
